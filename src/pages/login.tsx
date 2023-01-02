7fc29a5d7bb63377929450d08c3bf5e3a3d0dffc
Onno Schwanen <me@onnoschwanen.com>
2023-01-02 09:37:19 +0100

    Remove Login component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,43 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import type { GetServerSideProps, NextPage } from 'next';
+import Link from 'next/link';
+import { useState } from 'react';
 
 import Layout from '@/components/Layout';
 import LoginComponent from '@/components/Login';
+import RequestOtpForm from '@/components/RequestOtpForm';
+import VerifyOtpForm from '@/components/VerifyOtpForm';
 
 const Login: NextPage = () => {
+  const [email, setEmail] = useState<string>();
+
   return (
     <Layout title="Login">
-      <LoginComponent />
+      <Link href="/signup">Sign up</Link>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Login to Bookie
+            </h2>
+          </div>
+
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="rounded-lg bg-white py-8 px-4 shadow sm:px-10">
+              {email ? (
+                <VerifyOtpForm email={email} isSignUp={false} />
+              ) : (
+                <RequestOtpForm
+                  isSignUp={false}
+                  onSubmitSuccess={(_email: string) => {
+                    setEmail(_email);
+                  }}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
