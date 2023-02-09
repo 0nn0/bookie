b7a129e31861bd0f49ed85c421c52dc7f590a57a
Onno Schwanen <me@onnoschwanen.com>
2023-02-09 13:56:45 +0100

    Move propertyNav to footer with icons

--- src/components/PropertyNav.tsx
+++ src/components/PropertyNav.tsx
@@ -1,3 +1,5 @@
+import { CalendarDays, Home, Settings, Users } from 'lucide-react';
+
 import Tabs from './Tabs';
 
 const PropertyNav = ({
@@ -9,23 +11,36 @@ const PropertyNav = ({
 }) => {
   const tabs = [
     {
+      name: 'General',
+      href: `/properties/${propertyId}/general`,
+      current: true,
+      Icon: Home,
+    },
+    {
       name: 'Calendar',
       href: `/properties/${propertyId}/calendar`,
       current: true,
+      Icon: CalendarDays,
     },
     {
       name: 'Guests',
       href: `/properties/${propertyId}/guests`,
       current: false,
+      Icon: Users,
     },
     {
       name: 'Settings',
       href: `/properties/${propertyId}/settings`,
       current: false,
+      Icon: Settings,
     },
   ];
 
-  return <Tabs items={tabs} />;
+  return (
+    <div className="bottom-nav fixed left-0 bottom-0 z-30 flex w-full border border-t border-gray-200 bg-gray-50 bg-opacity-40 px-1 shadow backdrop-blur-md md:hidden">
+      <Tabs items={tabs} />
+    </div>
+  );
 };
 
 export default PropertyNav;
--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -9,23 +9,29 @@ function classNames(...classes) {
 const Tabs = ({ items = [] }) => {
   const { asPath } = useRouter();
   return (
-    <div className="border-b border-gray-200">
-      <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+    <div className="flex flex-1 border-b border-gray-200">
+      <nav className="flex flex-1" aria-label="Tabs">
         {items.map((tab) => {
           const isCurrent = asPath.startsWith(tab.href);
+          const { Icon } = tab;
 
           return (
             <Link key={tab.name} href={tab.href}>
               <a
                 className={classNames(
                   isCurrent
-                    ? 'border-indigo-500 text-indigo-600'
-                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                  'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
+                    ? ' text-indigo-600'
+                    : 'text-gray-500  hover:text-gray-700',
+                  'flex-column flex-1 items-center justify-center whitespace-nowrap py-4 text-sm font-medium'
                 )}
                 aria-current={isCurrent ? 'page' : undefined}
               >
-                {tab.name}
+                <div className="flex justify-center">
+                  <Icon />
+                </div>
+                <div className="flex justify-center">
+                  <span>{tab.name}</span>
+                </div>
               </a>
             </Link>
           );
