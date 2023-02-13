70a56c273308d746154d6c3434e0580b948f37ae
Onno Schwanen <me@onnoschwanen.com>
2023-02-13 16:55:39 +0100

    Add bottom padding to property-nav

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -9,35 +9,33 @@ function classNames(...classes) {
 const Tabs = ({ items = [] }) => {
   const { asPath } = useRouter();
   return (
-    <div className="flex flex-1 border-b border-gray-200">
-      <nav className="flex flex-1" aria-label="Tabs">
-        {items.map((tab) => {
-          const isCurrent = asPath.startsWith(tab.href);
-          const { Icon } = tab;
+    <nav className="flex flex-1" aria-label="Tabs">
+      {items.map((tab) => {
+        const isCurrent = asPath.startsWith(tab.href);
+        const { Icon } = tab;
 
-          return (
-            <Link key={tab.name} href={tab.href}>
-              <a
-                className={classNames(
-                  isCurrent
-                    ? ' text-indigo-600'
-                    : 'text-gray-500  hover:text-gray-700',
-                  'flex-column flex-1 items-center justify-center whitespace-nowrap py-4 text-sm font-medium'
-                )}
-                aria-current={isCurrent ? 'page' : undefined}
-              >
-                <div className="flex justify-center">
-                  <Icon />
-                </div>
-                <div className="mt-1 flex justify-center">
-                  <span>{tab.name}</span>
-                </div>
-              </a>
-            </Link>
-          );
-        })}
-      </nav>
-    </div>
+        return (
+          <Link key={tab.name} href={tab.href}>
+            <a
+              className={classNames(
+                isCurrent
+                  ? ' text-indigo-600'
+                  : 'text-gray-500  hover:text-gray-700',
+                'flex-column flex-1 items-center justify-center whitespace-nowrap py-4 text-sm font-medium'
+              )}
+              aria-current={isCurrent ? 'page' : undefined}
+            >
+              <div className="flex justify-center">
+                <Icon />
+              </div>
+              <div className="mt-1 flex justify-center">
+                <span>{tab.name}</span>
+              </div>
+            </a>
+          </Link>
+        );
+      })}
+    </nav>
   );
 };
 
