a24510845dfa210eedd2be0c9f751712f76c4db5
Onno Schwanen <me@onnoschwanen.com>
2023-02-15 11:10:56 +0100

    Add getServerSideProps auth check to new guest pahe

--- src/pages/properties/[id]/guests/new.tsx
+++ src/pages/properties/[id]/guests/new.tsx
@@ -1,20 +1,24 @@
-import { NextPage } from 'next';
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 import Card from '@/components/Card';
 import CardContent from '@/components/CardContent';
 import InviteGuestForm from '@/components/InviteGuestForm';
 import Layout from '@/components/Layout';
+import PropertyNav from '@/components/PropertyNav';
 import Container from '@/components/ui/Container';
 import Headline from '@/components/ui/Headline';
+import { Role, RoleId } from '@/pages/api/user';
 
-const New: NextPage = () => {
+const New: NextPage = ({ roleId }: { roleId: RoleId }) => {
   const { query } = useRouter();
   const propertyId = query.id as string;
 
   return (
     <Layout title="Invite guest">
       <Container>
+        <PropertyNav propertyId={propertyId} roleId={roleId} />
         <div className="mb-4">
           <Headline level={4}>Invite a guest</Headline>
         </div>
@@ -29,3 +33,62 @@ const New: NextPage = () => {
 };
 
 export default New;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+
+  if (!ctx.params?.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // check if user is owner of this property
+  const { data } = await supabase
+    .from('guests_owners')
+    .select()
+    .eq('profile_id', session.user.id)
+    .eq('property_id', ctx.params.id)
+    .eq('role_id', Role.OWNER)
+    .single();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // get role
+  const { data: roleData } = await supabase
+    .from('guests_owners')
+    .select('role_id')
+    .eq('profile_id', session.user.id)
+    .eq('property_id', ctx.params.id)
+    .single();
+
+  if (!roleData) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+      roleId: roleData.role_id,
+    },
+  };
+};
