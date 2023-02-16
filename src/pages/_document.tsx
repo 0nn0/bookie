7b85a55d524705dbc9052a2f9db781ee0874f3de
Onno Schwanen <me@onnoschwanen.com>
2023-02-16 10:20:05 +0100

    Add mobile app meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,14 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className="h-full bg-gray-100">
+    <Html lang="en" className="h-full bg-gray-100">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+        <meta name="apple-mobile-web-app-title" content="Bookie" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Bookie" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#94a3b8" />
         <link
           rel="preload"
