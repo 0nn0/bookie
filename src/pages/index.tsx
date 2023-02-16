30a24571080ebfe1790b4b4ef7a20c27df3a10a0
Onno Schwanen <me@onnoschwanen.com>
2023-02-16 16:05:01 +0100

    Update copy landingpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { GetServerSideProps, NextPage } from 'next';
+import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 
 import BookingList from '@/components/BookingList';
@@ -9,10 +9,8 @@ import SectionHeading from '@/components/SectionHeading';
 import Container from '@/components/ui/Container';
 import Headline from '@/components/ui/Headline';
 
-import { RoleId } from './api/user';
-
-const Home = ({ session }: { session: any }) => {
-  if (!session?.user)
+const Home = ({ initialSession }: { initialSession: any }) => {
+  if (!initialSession?.user)
     return (
       <Layout title="Home">
         <Container>
@@ -33,24 +31,45 @@ const Home = ({ session }: { session: any }) => {
             </nav>
           </header>
           <Headline level={1}>
-            Easily share your holiday home with friends and family
+            Effortlessly manage your vacation home with Bookie
           </Headline>
-          <p>
-            Bookie helps families manage collectively owned vacation homes. No
-            more manually planning who can stay when. Family and friends can
-            simply make reservations themselves based on your configuration.
+          <p className="mt-6">
+            Bookie is a user-friendly platform designed to assist families and
+            friends in managing their jointly owned vacation homes. With Bookie,
+            you no longer have to worry about manually coordinating who can stay
+            at the property and when. Your family and friends can easily make
+            their own reservations based on your predetermined configuration,
+            simplifying the entire booking process.
           </p>
           <p></p>
-          <div className="mt-10">
-            <Headline level={2}>Features</Headline>
-            <div className="">
-              <ul className="list-inside list-disc">
-                <li>
-                  Shared calendar through which you can manage all bookings
-                </li>
-                <li>Booking system configurable to your needs</li>
-                <li>Supports multiple properties</li>
-              </ul>
+          <div className="mt-12">
+            <Headline level={2}>Benefits</Headline>
+            <div className="mt-6 flex flex-col gap-6 md:flex-row">
+              <div>
+                <Headline level={5}>Easy scheduling</Headline>
+                <p>
+                  Bookie makes it easy for family and friends to schedule stays
+                  at the vacation home without a central coordinator.
+                </p>
+              </div>
+
+              <div>
+                <Headline level={5}>Organized reservations</Headline>
+                <p>
+                  Keep track of who has booked the property and when to prevent
+                  double bookings and ensure everyone has a fair opportunity to
+                  use it.
+                </p>
+              </div>
+
+              <div>
+                <Headline level={5}>Reduced stress</Headline>
+                <p>
+                  Simplifying the booking process and eliminating the need for a
+                  central coordinator can help reduce stress and potential
+                  conflicts.
+                </p>
+              </div>
             </div>
           </div>
         </Container>
@@ -69,7 +88,7 @@ const Home = ({ session }: { session: any }) => {
           <SectionHeading title="Upcoming bookings" />
         </div>
 
-        <BookingList userId={user.id} filter="UPCOMING" />
+        <BookingList userId={initialSession.user.id} filter="UPCOMING" />
 
         <div className="mt-16 mb-6">
           <SectionHeading
