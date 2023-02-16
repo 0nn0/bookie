a6797a43beebeb73a6bd88071ba63a4c3691c182
Onno Schwanen <me@onnoschwanen.com>
2023-02-16 16:07:01 +0100

    Remove unused types from useGetPropertiesQuery

--- src/hooks/useGetPropertiesQuery.ts
+++ src/hooks/useGetPropertiesQuery.ts
@@ -1,10 +1,9 @@
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import { useQuery } from '@tanstack/react-query';
 
-import { PropertiesByUser } from '@/pages/api/properties';
-
 const useGetPropertiesQuery = () => {
   const user = useUser();
+  console.log({ user });
   const supabaseClient = useSupabaseClient();
 
   const fetchProperties = async () => {
@@ -15,9 +14,10 @@ const useGetPropertiesQuery = () => {
       .throwOnError();
   };
 
-  return useQuery<unknown, unknown, PropertiesByUser>({
+  return useQuery({
     queryKey: ['properties'],
     queryFn: async () => fetchProperties().then((result) => result.data),
+    enabled: !!user,
   });
 };
 
