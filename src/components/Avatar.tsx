a5597bce15eebcafa91f85ca56bec6f9630ddc1e
Onno Schwanen <me@onnoschwanen.com>
2023-02-16 16:07:16 +0100

    Add fallback to avatar if no name provided yet

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -11,6 +11,7 @@ const Avatar = ({
   lastName: string;
   size?: number;
 }) => {
+  console.log({ avatarUrl, firstName, lastName, size });
   return (
     <div
       title={`${firstName} ${lastName}`}
@@ -26,7 +27,7 @@ const Avatar = ({
           className="focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
           alt={`Avatar of ${firstName} ${lastName}`}
         />
-      ) : (
+      ) : firstName ? (
         <svg viewBox="0 0 32 32" className="absolute h-full w-full">
           <rect x={0} y={0} width={32} height={32} className="fill-slate-300" />
           <text
@@ -40,6 +41,16 @@ const Avatar = ({
             {lastName && lastName[0]}
           </text>
         </svg>
+      ) : (
+        <span className="bg-gray-100">
+          <svg
+            className="h-full w-full text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        </span>
       )}
     </div>
   );
