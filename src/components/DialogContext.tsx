adb59656010a8fb66be5993c48009bd01ce2da0b
Onno Schwanen <me@onnoschwanen.com>
2023-02-23 12:20:21 +0100

    Fix size and padding dialog

--- src/components/DialogContext.tsx
+++ src/components/DialogContext.tsx
@@ -28,7 +28,7 @@ export function DialogProvider(props: { children: React.ReactNode }) {
         <Dialog
           as="div"
           className="relative z-10"
-          /*{initialFocus={cancelButtonRef}*/
+          // initialFocus={cancelButtonRef}
           onClose={setOpen}
         >
           <Transition.Child
@@ -44,7 +44,7 @@ export function DialogProvider(props: { children: React.ReactNode }) {
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-6 pb-20 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-end justify-center px-4 py-20 text-center sm:items-center sm:p-0">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -54,7 +54,7 @@ export function DialogProvider(props: { children: React.ReactNode }) {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <Dialog.Panel className="relative flex-1 transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                   {dialog}
                 </Dialog.Panel>
               </Transition.Child>
