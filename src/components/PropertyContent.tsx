13ab67823100ce3e171b8bb1235726bf1bbee6d1
Onno Schwanen <me@onnoschwanen.com>
2023-02-25 11:07:27 +0100

    Add comment to PropertyContent component

--- src/components/PropertyContent.tsx
+++ src/components/PropertyContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Add margin to the bottom to compensate for the fixed footer
 const PropertyContent = ({ children }) => {
   return <div className="mb-32">{children}</div>;
 };
