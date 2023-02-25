133873663fcedfd2bffb9e6d00b5a219bf8de69c
Onno Schwanen <me@onnoschwanen.com>
2023-02-25 11:07:12 +0100

    Fix NEXT_PUBLIC_SUPABASE_URL

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envVariables = z.object({
-  NEXT_PUBLIC_SUPABASEURL: z.string(),
+  NEXT_PUBLIC_SUPABASE_URL: z.string(),
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY: z.string(),
   NEXT_PUBLIC_BUCKET_NAME: z.string(),
