46802d46a53bc7392e3b3598ecb6007d4bb72c06
Onno Schwanen <me@onnoschwanen.com>
2023-04-21 14:22:37 +0200

    Remove layout from marketing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,67 +13,62 @@ import Headline from '@/components/ui/Headline';
 const Home = ({ initialSession }: { initialSession: any }) => {
   if (!initialSession?.user)
     return (
-      <Layout
-        title="Home"
-        description="Platform for managing your vacation home"
-      >
-        <Container>
-          <header className="mb-6">
-            <nav>
-              <ul className="flex gap-4">
-                <li>
-                  <Link href="/login">Login</Link>
-                </li>
-                <li>
-                  <Link href="/signup">Signup</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <Headline level={1}>
-            Effortlessly manage your vacation home with Bookie
-          </Headline>
-          <p className="mt-6">
-            Bookie is a user-friendly platform designed to assist families and
-            friends in managing their jointly owned vacation homes. With Bookie,
-            you no longer have to worry about manually coordinating who can stay
-            at the property and when. Your family and friends can easily make
-            their own reservations based on your predetermined configuration,
-            simplifying the entire booking process.
-          </p>
-          <p></p>
-          <div className="mt-12">
-            <Headline level={2}>Benefits</Headline>
-            <div className="mt-6 flex flex-col gap-6 md:flex-row">
-              <div>
-                <Headline level={5}>Easy scheduling</Headline>
-                <p>
-                  Bookie makes it easy for family and friends to schedule stays
-                  at the vacation home without a central coordinator.
-                </p>
-              </div>
+      <Container>
+        <header className="mb-6">
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+              <li>
+                <Link href="/signup">Signup</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Headline level={1}>
+          Effortlessly manage your vacation home with Bookie
+        </Headline>
+        <p className="mt-6">
+          Bookie is a user-friendly platform designed to assist families and
+          friends in managing their jointly owned vacation homes. With Bookie,
+          you no longer have to worry about manually coordinating who can stay
+          at the property and when. Your family and friends can easily make
+          their own reservations based on your predetermined configuration,
+          simplifying the entire booking process.
+        </p>
+        <p></p>
+        <div className="mt-12">
+          <Headline level={2}>Benefits</Headline>
+          <div className="mt-6 flex flex-col gap-6 md:flex-row">
+            <div>
+              <Headline level={5}>Easy scheduling</Headline>
+              <p>
+                Bookie makes it easy for family and friends to schedule stays at
+                the vacation home without a central coordinator.
+              </p>
+            </div>
 
-              <div>
-                <Headline level={5}>Organized reservations</Headline>
-                <p>
-                  Keep track of who has booked the property and when to prevent
-                  double bookings and ensure everyone has a fair opportunity to
-                  use it.
-                </p>
-              </div>
+            <div>
+              <Headline level={5}>Organized reservations</Headline>
+              <p>
+                Keep track of who has booked the property and when to prevent
+                double bookings and ensure everyone has a fair opportunity to
+                use it.
+              </p>
+            </div>
 
-              <div>
-                <Headline level={5}>Reduced stress</Headline>
-                <p>
-                  Simplifying the booking process and eliminating the need for a
-                  central coordinator can help reduce stress and potential
-                  conflicts.
-                </p>
-              </div>
+            <div>
+              <Headline level={5}>Reduced stress</Headline>
+              <p>
+                Simplifying the booking process and eliminating the need for a
+                central coordinator can help reduce stress and potential
+                conflicts.
+              </p>
             </div>
           </div>
-        </Container>
-      </Layout>
+        </div>
+      </Container>
     );
 
   const action = {
